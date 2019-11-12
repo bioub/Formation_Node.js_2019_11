400e5412a5bc6bc7a9c481b49434c0427e56d39f
Romain Bohdanowicz <romain.bohdanowicz@gmail.com>
2019-11-12 16:12:51 +0100

    Objet

---
+++ Langage/08-object.js
@@ -0,0 +1,75 @@
+// L'objet JavaScript pourrait s'appeler dictionnaire
+// -> c'est un système clé / valeur
+
+console.log(Math.PI);
+console.log(Math.random());
+
+console.log(Math.sum); // undefined
+
+Math.sum = (a, b) => a + b;
+console.log(Math.sum(1, 2));
+// Mauvaise pratique d'étendre les objets du langage/API Web/Node.js/bibliothèques externe
+
+delete Math.sum;
+
+// d'autres langages on aurait pu parler de :
+// PHP : tableau associatif
+// Java : Map
+// C : struct
+// Python : dictionary
+
+// Pour accéder au contenu de l'objet, 2 opérateurs
+console.log(Math.PI);
+console.log(Math.random());
+const key = 'PI';
+console.log(Math[key]);
+console.log(Math['random']());
+
+// Pour créer des objets, 2 syntaxes
+// object literal
+// -> pour des objets sans méthodes/fonctions
+// -> ou avec des méthodes s'il ne sont créé qu'un fois
+const coords1 = {
+  x: 1,
+  y: 2,
+  sumXY: function() { return this.x + this.y; }
+};
+const coords2 = {
+  x: 2,
+  y: 3,
+  sumXY: function() { return this.x + this.y; }
+};
+
+console.log(coords1.sumXY() === coords2.sumXY()); // false (3 !== 5)
+console.log(coords1.sumXY === coords2.sumXY); // false (2 fonctions)
+
+
+const MyMath = {
+  sum: (a, b) => a + b,
+};
+
+coords1.z = 3;
+delete coords1.y;
+
+console.log(coords1.x);
+
+// constructor function
+function Coords(x, y) {
+  this.x = x;
+  this.y = y;
+}
+
+Coords.prototype.sumXY = function() { return this.x + this.y; }
+
+const coordsA = new Coords(1, 2);
+const coordsB = new Coords(2, 3);
+console.log(typeof Coords); // function
+console.log(typeof coordsA); // object
+console.log(coordsA.x); // 1 (dans l'objet)
+console.log(coordsA.sumXY()); // undefined (dans l'objet) -> function (dans le prototype)
+console.log(coordsA.sumXY === coordsB.sumXY); // true
+
+
+// Objet Avancé
+// On peut empecher l'extension d'un objet
+// Object.preventExtensions(coordsB);
--- Langage/ex-jeu.js
+++ Langage/ex-jeu.js
@@ -1,23 +1,60 @@
+function getRandom() {
+  return Math.random();
+}
+
+function getRandomArbitrary(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}
+
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive
+}
+
 const readline = require('readline');
 
 const rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout,
 });
 
 function jouer() {
-  rl.question('What do you think of Node.js? ', (answer) => {
-    // TODO: Log the answer in a database
-    console.log(`Thank you for your valuable feedback: ${answer}`);
+  if (essais.length) {
+    console.log('Vous avez déjà joué : ' + essais.join(' - '));
+  }
 
-    // pour rejouer
-    // jouer();
+  rl.question('Devinez le nombre : ', (answer) => {
+    const entierSaisi = parseInt(answer);
 
-    // pour terminer
-    // rl.close();
+    if (isNaN(entierSaisi)) {
+      console.log('Erreur il faut saisir un entier');
+      return jouer();
+    }
+
+    essais.push(entierSaisi);
+
+    if (entierSaisi < entierAlea) {
+      console.log('Trop petit');
+      return jouer();
+    } else if (entierSaisi > entierAlea) {
+      console.log('Trop grand');
+      return jouer();
+    } else {
+      console.log('Gagné');
+      rl.close();
+    }
   });
 }
 
+const entierAlea = getRandomIntInclusive(0, 100);
+const essais = [];
 jouer();
 
 // pile d'appels

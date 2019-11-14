9c7d0cd467115931f800ac98d649c774922b15b7
Romain Bohdanowicz <romain.bohdanowicz@gmail.com>
2019-11-14 12:28:43 +0100

    Webpack

---
+++ Webpack/webpack.config.js
@@ -0,0 +1,30 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+/** @type {import('webpack').Configuration} */
+const config = {
+  devtool: false,
+  entry: './src/js/index.js',
+  output: {
+    filename: 'app.[chunkHash].js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
+};
+
+module.exports = config;

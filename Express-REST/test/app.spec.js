752f12c69cf512550a11bac1416265d6075d68c5
Romain Bohdanowicz <romain.bohdanowicz@gmail.com>
2019-11-15 15:24:52 +0100

    Fin de formation

--- Express-REST/test/app.spec.js
+++ Express-REST/test/app.spec.js
@@ -18,7 +18,47 @@ describe('Functionnal Tests', () => {
       const res = await chai.request(app).get('/api/contacts');
 
       expect(res).to.have.status(200);
+      expect(res.body).to.deep.equal([]);
       mock.restore();
+    });
+  });
+
+  // Faire le test de GET /api/contacts/123
+  // Utiliser sinon.mock pour générer une fausse fonction findById qui resolves
+  // un contact dont l'id est 123
+  describe('GET /api/contacts/123', () => {
+    let mock;
+
+    beforeEach(() => {
+      mock = sinon.mock(Contact);
+    });
+
+    afterEach(() => {
+      mock.restore();
+    });
+
+    it('should respond contact with status 200', async () => {
+      mock
+        .expects('findById')
+        .resolves({ firstName: 'Toto' })
+        .calledOnceWithExactly('123');
+
+      const res = await chai.request(app).get('/api/contacts/123');
+
+      expect(res).to.have.status(200);
+      expect(res.body).to.deep.equal({ firstName: 'Toto' });
+    });
+
+    it('should respond status 404 when contact is missing', async () => {
+      mock
+        .expects('findById')
+        .resolves(null)
+        .calledOnceWithExactly('123');
+
+      const res = await chai.request(app).get('/api/contacts/123');
+
+      expect(res).to.have.status(404);
+      expect(res.body).to.deep.equal({ msg: 'Contact 123 not found' });
     });
   });
 });
